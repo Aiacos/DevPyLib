7ce2e6f37da16c700afb4221687105f73a1a5c98
lorenzoargentieri <uni.lorenzo.a@gmail.com>
2017-02-01 23:28:36 +0100

    TwistJoint mid
    missing constraint, maybe convert to PyNode new joint

--- Python/rigLib/twistJoint.py
+++ Python/rigLib/twistJoint.py
@@ -10,14 +10,16 @@ tail_jnt = joint_selection[1]
 
 distance = util.getDistance(head_jnt, tail_jnt)
 
+pm.parent(tail_jnt.name(), world=True)
 twist_jnt_grp = pm.group(name=head_jnt.name() + '_twist' + '_grp')
 joint_list = []
 for i in range(0, n_twist_joint):
-    joint_name = head_jnt.name() + '_twist' + str(i+1)
+    joint_name = head_jnt.name() + '_twist' + str(i + 1)
     new_joint_name = pm.insertJoint(head_jnt.name())
     new_joint = pm.PyNode(str(new_joint_name))
 
     joint_list.append(new_joint)
-    pm.move((i+1)*distance,0,0, new_joint, relative=True, worldSpaceDistance=True)
-    pm.parent(tail_jnt, head_jnt)
+    pm.move((i + 1) * distance, 0, 0, new_joint, relative=True, localSpace=True)
     pm.parent(joint_list[i], twist_jnt_grp)
+
+pm.parent(tail_jnt, head_jnt)
