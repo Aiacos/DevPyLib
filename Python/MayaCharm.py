70353b8caeae534040c325a27ef1ab5ee3a9a498
lorenzoargentieri <uni.lorenzo.a@gmail.com>
2017-02-02 21:21:47 +0100

    MayaCharm

---
+++ Python/MayaCharm.py
@@ -0,0 +1,14 @@
+import sys
+import maya.cmds as cmds
+
+
+libDir = '/Users/lorenzoargentieri/Dropbox/3D/Maya/Script_DEF/DevPyLib/Python'
+port = ':5000'
+
+# Open Maya port
+if not cmds.commandPort(port, q=True):
+    cmds.commandPort(n=port)
+
+# Add develpment PATH
+if not libDir in sys.path:
+    sys.path.append(libDir)
