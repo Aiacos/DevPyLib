4bb0703f883b594418dffa36a6cbace7fb68b3c4
lorenzoargentieri <uni.lorenzo.a@gmail.com>
2017-02-03 19:48:36 +0100

    Reorganized ColorControl

---
+++ Python/rigLib/base/colorControl.py
@@ -0,0 +1,24 @@
+import pymel.core as pm
+
+
+def color_control(controls=pm.ls('*_ctrl*', type='nurbsCurve')):
+    """
+    Color all selected control based on theis prefix (L_ , R_)
+    :param controls:
+    :return:
+
+    Usage: color_control(controls=pm.ls('*_ctrl*', type='nurbsCurve'))
+    """
+    for ctrlObject in controls:
+        name = ctrlObject.name().encode('utf8')
+        pm.setAttr(name + '.ove', 1)
+        if name.startswith('L_'):
+            pm.setAttr(name + '.ovc', 6)
+        elif name.startswith('R_'):
+            pm.setAttr(name + '.ovc', 13)
+        else:
+            pm.setAttr(name + '.ovc', 22)
+
+
+if __name__ == "__main__":
+    color_control()
