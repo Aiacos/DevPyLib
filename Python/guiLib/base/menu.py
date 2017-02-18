4923e0da21159d935b70c5695c56a31310b66407
lorenzoargentieri <uni.lorenzo.a@gmail.com>
2017-02-18 23:38:50 +0100

    submenu

--- Python/guiLib/base/menu.py
+++ Python/guiLib/base/menu.py
@@ -9,23 +9,39 @@ class Menu():
 
     def __init__(self, menu_name='test', parent=gMainWindow):
         self.lib_menu = pm.menu(label=menu_name, parent=parent, tearOff=True)
-        self.item = []
+        self.item = {}
 
     def __del__(self):
         self.lib_menu.delete()
 
-    def add_menuitem(self, item_name, fn):
+    def add_menuitem(self, item_name, cmd, parent=None):
         """
         Bind function to menuItem
         :param item_name: Label for menuItem (String)
-        :param fn: pointer to function (es. def x(); y=x; y() <- this call x())
+        :param cmd: pointer to function (es. def x(); y=x; y() <- this call x())
+        :param parent: (String)
         :return:
         """
-        current_item = pm.menuItem(item_name, parent=self.lib_menu.name(), command=fn.__name__+'()')
-        self.item.append(current_item)
-        return current_item
+        if parent is None:
+            parent = self.lib_menu.name()
 
-    # ToDo: submenu
+        self.item[item_name] = pm.menuItem(item_name, p=parent, command=cmd.__name__ + '()')
+        ret_parent = self.item[item_name]
+        return ret_parent.rpartition('|')[-1]
+
+    def add_submenu(self, submenu_name, parent=None):
+        """
+        Add sub menu container
+        :param submenu_name: Label for Sub_menuItem (String)
+        :param parent: (String)
+        :return:
+        """
+        if parent is None:
+            parent = self.lib_menu.name()
+
+        self.item[submenu_name] = pm.menuItem(submenu_name, p=parent, subMenu=True, tearOff=True)
+        ret_parent = self.item[submenu_name]
+        return ret_parent.rpartition('|')[-1]
 
 
 def print_text():
@@ -34,7 +50,9 @@ def print_text():
 
 if __name__ == "__main__":
     menuPanel = Menu('test')
-    menuPanel.add_menuitem('testClickCmd', fn=print_text)
+    menuPanel.add_menuitem('testClickCmd', cmd=print_text)
+    p = menuPanel.add_submenu('testSubMenu')
+    menuPanel.add_menuitem('testSubItem', parent=p, cmd=print_text)
     # try:
     #     lib_menu.delete()
     #     print 'deleted'
