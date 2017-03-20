08013e4ad5d5769279f44a6c522b3ad029a97b89
lorenzoargentieri <uni.lorenzo.a@gmail.com>
2017-03-20 23:13:56 +0100

    mayacharm.py with reload

--- mayaLib/utility/maya_charm.py
+++ mayaLib/utility/maya_charm.py
@@ -3,8 +3,9 @@ __author__ = 'Lorenzo Argentieri'
 import sys
 import maya.cmds as cmds
 
-libDir = '/Users/lorenzoargentieri/Dropbox/3D/Maya/Script_DEF/DevPyLib/Python'
+libDir = '/Users/lorenzoargentieri/Dropbox/3D/Maya/Script_DEF/DevPyLib'
 port = ':5000'
+libName = 'mayaLib'
 
 # Open Maya port
 if not cmds.commandPort(port, q=True):
@@ -13,3 +14,6 @@ if not cmds.commandPort(port, q=True):
 # Add develpment PATH
 if not libDir in sys.path:
     sys.path.append(libDir)
+else:
+    reload(__import__(libName))
+    print 'Module: "' + libName + '" reloaded'
