7bdce3d4c8e210bfc187aae960d2c248b46cee36
lorenzoargentieri <uni.lorenzo.a@gmail.com>
2017-05-19 12:57:01 +0200

    DirectX/OpenGl normal switch

--- mayaLib/shaderLib/base/renderman.py
+++ mayaLib/shaderLib/base/renderman.py
@@ -70,9 +70,9 @@ class PxrSurface_shaderBase(Shader_base):
     def connect_luminance_pxrtexture(self, pxrtexture_node, slot_name):
         pm.connectAttr(pxrtexture_node.resultA, '%s.%s' % (self.shader, slot_name))
 
-    def connect_pxrnormal(self, pxrtexture_node, slot_name):
+    def connect_pxrnormal(self, pxrtexture_node, slot_name, directx_normal=False):
         self.pxrnormalmap_node = pm.shadingNode("PxrNormalMap", asTexture=True)
-        self.pxrnormalmap_node.invertBump.set(True)
+        self.pxrnormalmap_node.invertBump.set(directx_normal)
         pm.connectAttr(pxrtexture_node.resultRGB, self.pxrnormalmap_node.inputRGB)
         pm.connectAttr(self.pxrnormalmap_node.resultN, '%s.%s' % (self.shader, slot_name))
 
