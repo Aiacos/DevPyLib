ad9b6f1c6763916eb83fe3fcc6c97007ea76d14d
lorenzoargentieri <uni.lorenzo.a@gmail.com>
2017-07-22 10:36:51 +0100

    wingIDE connection

--- mayaLib/utility/maya_charm.py
+++ mayaLib/utility/maya_charm.py
@@ -7,6 +7,13 @@ libDir = '/Users/lorenzoargentieri/Dropbox/3D/Maya/Script_DEF/DevPyLib'
 port = ':5000'
 libName = 'mayaLib'
 
+# Wing IDE
+wingide_stub = '/Users/lorenzoargentieri/Dropbox/3D/Maya/Script_DEF/DevPyLib/wingIDE'
+if wingide_stub not in sys.path: sys.path.append(wingide_stub)
+
+import wingdbstub
+wingdbstub.Ensure()
+
 # Open Maya port
 if not cmds.commandPort(port, q=True):
     cmds.commandPort(n=port)
