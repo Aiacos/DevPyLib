1d183aaefaedf9d7d582deb6166217b4b959eb34
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2018-01-22 22:59:43 +0000

    copyBind

--- mayaLib/rigLib/utils/skin.py
+++ mayaLib/rigLib/utils/skin.py
@@ -16,6 +16,36 @@ def copySkinWeightBetweenMesh(selection=pm.ls(sl=True)):
     pm.copySkinWeights(ss=sourceSkinCluster, ds=destinationSkinCluster, mirrorMode='YZ',
                        surfaceAssociation='closestPoint', influenceAssociation='closestJoint')
 
+def copyBind(source, destination):
+    """
+    Bind and Copy skincluster to destination GEO
+    :param source: mesh str
+    :param destination: mesh str
+    :return: 
+    """
+    # Get Shape and skin from Object
+    shape = pm.ls(source)[0].getShape()
+    skinCluster = pm.listConnections(shape + '.inMesh', destination=False)
+    if len(skinCluster) > 0:
+        skin = pm.PyNode(skinCluster[0])
+    else:
+        print 'Missing source SkinCluster'
+
+    # Get joint influence of the skin
+    influnces = skin.getInfluence(q=True)  # influences is joint
+
+    # Bind destination Mesh
+    #pm.select(influnces[0])
+    #pm.select(destination, add=True)
+    #mel.eval('SmoothBindSkin;')
+    pm.skinCluster(influnces[0], destination, dr=4.0)
+
+    # copy skin wheights form source
+    pm.select(source)
+    pm.select(destination, add=True)
+    pm.copySkinWeights(noMirror=True, surfaceAssociation='closestPoint', influenceAssociation='closestJoint')
+    pm.select(cl=True)
+
 if __name__ == "__main__":
     copySkinWeightBetweenMesh()
     print 'Done!'
