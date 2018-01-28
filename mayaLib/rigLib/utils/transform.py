4b48af9c4bbaae9e90f9d755aee9f5a1c3d95487
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2018-01-28 11:40:49 +0000

    utils makeModifyGrp

--- mayaLib/rigLib/utils/transform.py
+++ mayaLib/rigLib/utils/transform.py
@@ -35,3 +35,32 @@ def makeOffsetGrp(object, prefix=''):
     pm.parent(object, offsetGrp)
 
     return offsetGrp
+
+
+def makeModifyGrp(object, prefix=''):
+    """
+    make modify group for given object
+
+    @param object: transform object to get modify group
+    @param prefix: str, prefix to name new objects
+    @return: str, name of new modify group
+    """
+
+    if not prefix:
+        prefix = name.removeSuffix(object)
+
+    modifyGrp = pm.group(n=prefix + 'Modify_GRP', em=1)
+
+    objectParents = pm.listRelatives(object, p=1)
+
+    if objectParents:
+        pm.parent(modifyGrp, objectParents[0])
+
+    # match object transform
+    pm.delete(pm.parentConstraint(object, modifyGrp))
+    pm.delete(pm.scaleConstraint(object, modifyGrp))
+
+    # parent object under offset group
+    pm.parent(object, modifyGrp)
+
+    return modifyGrp
