6fc745467e2e9aabd7a38f2d3c39562698d76296
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2018-01-28 22:10:01 +0000

    refactor flexiplane

--- mayaLib/rigLib/base/flexiplane.py
+++ mayaLib/rigLib/base/flexiplane.py
@@ -2,6 +2,7 @@ __author__ = 'Lorenzo Argentieri'
 
 import pymel.core as pm
 from string import letters
+from mayaLib.pipelineLib.utility import nameCheck
 from mayaLib.shaderLib.base import shader_base
 from mayaLib.rigLib.utils import util
 
@@ -20,13 +21,13 @@ class Flexiplane():
     }
 
     def __init__(self, prefix=''):
-        self.fp = 'flexiPlane_'
-        self.sur = 'surface_'
+        self.fp = 'flexiPlane*'
+        self.surfaceSuffix = 'NURBS'
         self.YELLOW = 17
 
         Flexiplane.SETTINGS_DEFAULT['prefix'] = prefix
         Flexiplane.SETTINGS_DEFAULT['num'] += 1
-        self.flexiplane(settings=Flexiplane.SETTINGS_DEFAULT)
+        self.flexiplane(prefix=prefix)
 
     def safe_delete(self):
         """
@@ -46,9 +47,6 @@ class Flexiplane():
         delete_flex = [pm.delete(f.getParent()) for f in flex]
         return flex, curve_name
 
-    def test(self):
-        print Flexiplane.SETTINGS_DEFAULT['prefix']
-        print Flexiplane.SETTINGS_DEFAULT['num']
 
     def create_plane(self, name, width=10, lengthratio=0.2):
         """
@@ -75,7 +73,7 @@ class Flexiplane():
         :param transparency: es: (0,1,0)
         :return: none
         """
-        name = 'flexiPlane_lambert_material01'
+        name = 'flexiPlane_lambert_material'
         my_shader = shader_base.build_lambert(shaderType='lambert',
                                               shaderName=name,
                                               color=color,
@@ -89,7 +87,7 @@ class Flexiplane():
         :param color: es: (0,1,0)
         :return: none
         """
-        name = 'flexiPlane_surface_material01'
+        name = 'flexiPlane_surface_material'
         my_shader = shader_base.build_surfaceshader(shaderType='surfaceShader',
                                                     shaderName=name,
                                                     color=color)
@@ -188,19 +186,19 @@ class Flexiplane():
         :return: new clusters
         """
         cl_a = self.make_cluster(target=curve.cv[0:1],
-                                 name='%s_cl_a01' % name,
+                                 name='%s_a_CL' % name,
                                  origin=-6,
                                  pos_x=-5,
                                  pos_z=-5)
 
         cl_b = self.make_cluster(target=curve.cv[1:2],
-                                 name='%s_cl_b01' % name,
+                                 name='%s_b_CL' % name,
                                  origin=6,
                                  pos_x=5,
                                  pos_z=-5)
 
         cl_mid = self.make_cluster(target=curve.cv[1],
-                                   name='%s_cl_mid01' % name)
+                                   name='%s_mid_CL' % name)
 
         # redistributes cluster weight
         pm.percent(cl_a[0], curve.cv[1], v=0.5)
@@ -225,7 +223,7 @@ class Flexiplane():
         return mid_ctrl
 
     # function to create global move control curve
-    def global_ctrl(self, name='ctrl', settings=SETTINGS_DEFAULT):
+    def global_ctrl(self, name='ctrl'):
         """
         create global control curve
         :param name: string for control curve name
@@ -238,7 +236,7 @@ class Flexiplane():
                              r=0.3,
                              nr=[0, 1, 0],
                              ch=0,
-                             n='%sglobal_%i' % (name, settings['num']))[0]
+                             n='%s_global_CTRL' % (name))[0]
         # grab its shape and recolors it
 
         glb_ctrlshape = glb_ctrl.getShape()
@@ -261,7 +259,7 @@ class Flexiplane():
                                r=0.3,
                                nr=[0, 1, 0],
                                ch=0,
-                               n='%sglobal_b_%i' % (name, settings['num']))[0]
+                               n='%sglobal_b_CTRL' % (name))[0]
         # grabs it's shape recolors it
         glb_ctrl_bshape = glb_ctrl_b.getShape()
         glb_ctrl_bshape.overrideEnabled.set(1)
@@ -278,49 +276,50 @@ class Flexiplane():
                   r=True)
         return glb_ctrl
 
-    def flexiplane(self, settings=SETTINGS_DEFAULT):
+    def flexiplane(self, prefix=''):
         """
         Build FlexiPlane
         :param index: number of flexiplane in scene (auto managed by maya)
         :return: FlexiPlane group node
         """
 
-        fp_name = '%sflexiPlane_' % settings['prefix']
+        fp_name = '%sflexiPlane' % prefix
+        fp_name = nameCheck.nameCheck(fp_name+'*_GRP').replace('_GRP', '', 1)# flexiPlane_GRP
 
-        fp_surf = self.create_plane('%s%s_%i' % (fp_name, self.sur, settings['num']))[0]
+        fp_surf = self.create_plane('%s%s' % (fp_name, self.surfaceSuffix))[0]
 
         # Assign Material
         self.create_lambret(fp_surf, color=(0.067, 0.737, 0.749), transparency=(0.75, 0.75, 0.75))
 
         # Create Follicles
-        flc_name = 'flexiPlane'
+        #flc_name = 'flexiPlane'
         v = 0.1  # 1/width
         flcs = []
         how_many_flc = 5  # width/2
         for i in range(0, how_many_flc):
-            ofoll = self.create_follicle(fp_surf, '%sflc_%s_%i' % (fp_name, letters[i + 26], settings['num']), v, 0.5)
+            ofoll = self.create_follicle(fp_surf, '%s_flc_%s_FLC' % (fp_name, letters[i + 26]), v, 0.5)
             flcs.append(ofoll)
             v += 0.2  # (1/width)*2
 
         # Group Follicles
-        flc_grp = pm.group(flcs, name='%sflcs_%i' % (fp_name, settings['num']))
+        flc_grp = pm.group(flcs, name='%s_flcs_GRP' % (fp_name))
 
         # creates flexiPlane controls curves at each end
-        ctrl_a = self.ctrl_square(name='%sctrl_a_%i' % (fp_name, settings['num']), pos=[-5, 0, 0])
+        ctrl_a = self.ctrl_square(name='%s_ctrl_a_CTRL' % (fp_name), pos=[-5, 0, 0])
         ctrl_ashape = ctrl_a.getShape()
         pm.rename(ctrl_ashape, '%sShape' % ctrl_a)
 
-        ctrl_b = self.ctrl_square(name='%sctrl_b_%i' % (fp_name, settings['num']), pos=[5, 0, 0])
+        ctrl_b = self.ctrl_square(name='%s_ctrl_b_CTRL' % (fp_name), pos=[5, 0, 0])
         ctrl_bshape = ctrl_b.getShape()
         pm.rename(ctrl_bshape, '%sShape' % ctrl_b)
 
         pm.select(cl=True)
 
         # creates flexiPlane blendshape     #  blendshape suffix: _bShp_
-        fp_bshp = pm.duplicate(fp_surf, n='%sbshp_%s_%i' % (fp_name, self.sur, settings['num']))[0]
+        fp_bshp = pm.duplicate(fp_surf, n='%s_bshp_NURBS' % (fp_name))[0]
         pm.move(0, 0, -5, fp_bshp)
 
-        fps_bshp_node = pm.blendShape(fp_bshp, fp_surf, n='%sbshpNode_%s%i' % (fp_name, self.sur, settings['num']))[0]
+        fps_bshp_node = pm.blendShape(fp_bshp, fp_surf, n='%s_BSHP' % (fp_name))[0]
         pm.setAttr('%s.%s' % (fps_bshp_node, fp_bshp), 1)
         # pm.rename('tweak1', '%sbshp_%stweak_01' % (fp_name, sur))
 
@@ -328,7 +327,7 @@ class Flexiplane():
         fp_curve = pm.curve(d=2,
                             p=[(-5, 0, -5), (0, 0, -5), (5, 0, -5)],
                             k=[0, 0, 1, 1],
-                            n='%swire_%s%i' % (fp_name, self.sur, settings['num']))
+                            n='%s_wire_CV' % (fp_name))
         cl_a, cl_b, cl_mid = self.cluster_curve(fp_curve, fp_name)
 
         # create and place twist deformer
@@ -336,8 +335,8 @@ class Flexiplane():
         fp_twist = pm.nonLinear(type='twist', lowBound=-1, highBound=1)
         # displays warning: pymel.core.general : could not create desired mfn. Defaulting MfnDependencyNode.
         # doesn't seem to affect anything though
-        pm.rename(fp_twist[0], '%stwistAttr_surface_%i' % (fp_name, settings['num']))
-        pm.rename(fp_twist[1], '%stwist_%i_Handle' % (fp_name, settings['num']))
+        pm.rename(fp_twist[0], '%s_twistAttr_surface_NURBS' % (fp_name))
+        pm.rename(fp_twist[1], '%s_twist_Handle_DEFORMER' % (fp_name))
         fp_twist[1].rz.set(90)
         # connect start and end angle to their respective control
         connect = ctrl_b.rx >> fp_twist[0].startAngle
@@ -345,25 +344,25 @@ class Flexiplane():
 
         # skins wire to blendshape
         fp_wire = pm.wire(fp_bshp, w=fp_curve, gw=False, en=1, ce=0, li=0,  # dds=(0, 20),
-                          n='%swireAttrs_%s%i' % (fp_name, self.sur, settings['num']))
+                          n='%s_wireAttrs_DEFORMER' % (fp_name))
         fp_wire[0].dropoffDistance[0].set(20)
         hist = pm.listHistory(fp_surf)
         tweaks = [t for t in hist if 'tweak' in t.nodeName()]
-        pm.rename(tweaks[2], '%scl_cluster_tweak_%i' % (fp_name, settings['num']))
-        pm.rename(tweaks[0], '%swireAttrs_%stweak_%i' % (fp_name, self.sur, settings['num']))
-        pm.rename(tweaks[1], '%sextra_%stweak_%i' % (fp_name, self.sur, settings['num']))
+        pm.rename(tweaks[2], '%s_cl_cluster_tweak' % (fp_name))
+        pm.rename(tweaks[0], '%s_wireAttrs_tweak' % (fp_name))
+        pm.rename(tweaks[1], '%s_extra_tweak' % (fp_name))
 
         # group clusters
-        cl_grp = pm.group(cl_a[1], cl_b[1], cl_mid[1], n='%scls_%i' % (fp_name, settings['num']))
+        cl_grp = pm.group(cl_a[1], cl_b[1], cl_mid[1], n='%s_cls_GRP' % (fp_name))
         util.lock_and_hide_all(cl_grp)
 
         # creates mid control
-        ctrl_mid = self.flexiplane_mid_ctrl(name='%sctrl_mid_%i' % (fp_name, settings['num']))
-        ctrl_mid_grp = pm.group(ctrl_mid, n='%sgrp_midBend_%i' % (fp_name, settings['num']))
+        ctrl_mid = self.flexiplane_mid_ctrl(name='%s_ctrl_mid_CTRL' % (fp_name))
+        ctrl_mid_grp = pm.group(ctrl_mid, n='%s_grp_midBend_GRP' % (fp_name))
         pm.pointConstraint(ctrl_a, ctrl_b, ctrl_mid_grp, o=[0, 0, 0], w=1)
 
         # groups controls together and locks and hides group attributes
-        ctrl_grp = pm.group(ctrl_a, ctrl_b, ctrl_mid_grp, n='%sctrl_%i' % (fp_name, settings['num']))
+        ctrl_grp = pm.group(ctrl_a, ctrl_b, ctrl_mid_grp, n='%s_ctrl_GRP' % (fp_name))
         util.lock_and_hide_all(ctrl_grp)
 
         # connecting translate attrs of control curves for to the clusters
@@ -379,16 +378,20 @@ class Flexiplane():
 
         # groups everything under 1 group then locks and hides the transform attrs of that group #flexiPlane_wire_surface0101BaseWire
         fp_grp = pm.group(fp_surf, flc_grp, fp_bshp, fp_wire,
-                          '%swire_%s%iBaseWire' % (fp_name, self.sur, settings['num']),
-                          cl_grp, ctrl_grp, n='%s%i' % (fp_name, settings['num']))
+                          #'%s_wire_%s_BaseWire_GRP' % (fp_name, self.surfaceSuffix),
+                          cl_grp, ctrl_grp, n='%s_GRP' % (fp_name))
         util.lock_and_hide_all(fp_grp)
 
         # creates global move group and extraNodes
-        fp_gm_grp = pm.group(fp_surf, ctrl_grp, n='%sglobalMove_%i' % (fp_name, settings['num']))
+        fp_gm_grp = pm.group(fp_surf, ctrl_grp, n='%s_globalMove_GRP' % (fp_name))
         fp_xnodes_grp = pm.group(flc_grp, fp_bshp, fp_wire,
-                                 '%swire_%s%iBaseWire' % (fp_name, self.sur, settings['num']),
-                                 cl_grp, n='%sextraNodes_%i' % (fp_name, settings['num']))
+                                 '%s_wire_CVBaseWire' % (fp_name),
+                                 cl_grp, n='%s_extraNodes_GRP' % (fp_name))
         pm.parent(fp_twist, fp_xnodes_grp)
+        pm.parent(fp_xnodes_grp, fp_grp)
+        fp_xnodes_grp.overrideEnabled.set(1)
+        fp_xnodes_grp.overrideDisplayType.set(2)
+
 
         # scale constrains follicles to global move group
         for follicle in flcs:
@@ -396,7 +399,7 @@ class Flexiplane():
             pm.scaleConstraint(fp_gm_grp, mparent)
 
         # creates global move control
-        fp_gm_ctrl = self.global_ctrl(name='%sctrl_' % fp_name, settings=settings)
+        fp_gm_ctrl = self.global_ctrl(name='%s_CTRL' % fp_name)
 
         # moves global control into flexiPlane group then parent global move group to global move control.
         pm.parent(fp_gm_ctrl, fp_grp)
@@ -406,7 +409,7 @@ class Flexiplane():
         jnts = []
         for i in range(0, len(flcs)):
             posx = round(flcs[i].getParent().translateX.get(), 4)
-            jnt = pm.joint(p=(posx, 0, 0), rad=0.5, n='%sbind_%s_%i' % (fp_name, letters[i + 26], settings['num']))
+            jnt = pm.joint(p=(posx, 0, 0), rad=0.5, n='%sbind_%s_JNT' % (fp_name, letters[i + 26]))
             jnts.append(jnt)
             # parent joint under follicle
             pm.parent(jnt, flcs[i].getParent())
@@ -423,20 +426,20 @@ class Flexiplane():
         # ...to get the wire deformers length
         pm.select(fp_curve, r=True)
         length = pm.arclen(ch=1)
-        length.rename('%scurveInfo_%i' % (fp_name, settings['num']))
+        length.rename('%scurveInfo_DEFORMER' % (fp_name))
 
         # creates a multiplyDivideNode for squashStretch length...
         # ...and sets it operation to divide
-        fp_div = pm.createNode('multiplyDivide', n='%sdiv_squashStretch_length_%i' % (fp_name, settings['num']))
+        fp_div = pm.createNode('multiplyDivide', n='%sdiv_squashStretch_length' % (fp_name))
         fp_div.operation.set(2)
 
         # secondary multDivNode for volume, sets input1X to 1
-        fp_div_vol = pm.createNode('multiplyDivide', n='%sdiv_volume_%i' % (fp_name, settings['num']))
+        fp_div_vol = pm.createNode('multiplyDivide', n='%sdiv_volume' % (fp_name))
         fp_div_vol.operation.set(2)
         fp_div_vol.input1X.set(1)
 
         # creates a conditionNode for global_ctrl enable attr
-        fp_cond = pm.createNode('condition', n='%scond_volume_%i' % (fp_name, settings['num']))
+        fp_cond = pm.createNode('condition', n='%scond_volume' % (fp_name))
         fp_cond.secondTerm.set(1)
 
         # connects curve all the nodes
