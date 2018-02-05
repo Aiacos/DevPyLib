9fd226af29786d411be2f827cea6691a8ef66c2f
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2018-02-05 20:19:18 +0000

    init shrinkWrap deformer

--- mayaLib/rigLib/utils/deform.py
+++ mayaLib/rigLib/utils/deform.py
@@ -26,6 +26,17 @@ def deltaMushDeformer(geo):
     deformerNode = pm.deltaMush(geo, smoothingIterations=25)[0]
     return deformerNode
 
+def shrinkWrapDeformer(wrappedObj, wrapperObj):
+    """
+    Apply Wrap Deformer on selected mesh
+    :param wrappedObjs: str
+    :param wrapperObj: str
+    :return: deformer node
+    """
+    shrinkWrapNode = pm.deformer(wrappedObj, type='shrinkWrap')[0]
+    pm.PyNode(wrapperObj).worldMesh[0] >> shrinkWrapNode.targetGeom
+    shrinkWrapNode.closestIfNoIntersection.set(True)
+    return shrinkWrapNode
 
 if __name__ == "__main__":
     pass
