9ae9b67413daf44b2e550354c8934519d3f02c50
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2018-02-15 20:31:20 +0000

    copyShape, ctrlShape update

--- mayaLib/rigLib/utils/ctrlShape.py
+++ mayaLib/rigLib/utils/ctrlShape.py
@@ -3,6 +3,20 @@ __author__ = 'Lorenzo Argentieri'
 import pymel.core as pm
 from mayaLib.rigLib.utils import common
 
+
+def copyShape(source, destination):
+    """
+    Copy Shape from source to destination transform
+    :param source: obj
+    :param destination: obj
+    :return:
+    """
+    sourceShapes = source.getShapes()
+    destinationShapes = destination.getShapes()
+    pm.delete(destinationShapes)
+    pm.parent(sourceShapes, destination, r=True, s=True)
+    pm.delete(source)
+
 def moveCtrlShape(name, scale):
     cv = pm.curve(n=name, d=1, p=[(1, 0, -3), (2, 0, -3), (0, 0, -5), (-2, 0, -3), (-1, 0, -3), (-1, 0, -2), (-1, 0.5, -1.5), (-1.35, 0.5, -1.35), (-1.5, 0.5, -1), (-2, 0, -1), (-3, 0, -1), (-3, 0, -2), (-5, 0, 0), (-3, 0, 2), (-3, 0, 1), (-2, 0, 1), (-1.5, 0.5, 1), (-1.35, 0.5, 1.35), (-1, 0.5, 1.5), (-1, 0, 2), (-1, 0, 3), (-2, 0, 3), (0, 0, 5), (2, 0, 3), (1, 0, 3), (1, 0, 2), (1, 0.5, 1.5), (1.35, 0.5, 1.35), (1.5, 0.5, 1), (2, 0, 1), (3, 0, 1), (3, 0, 2), (5, 0, 0), (3, 0, -2), (3, 0, -1), (2, 0, -1), (1.5, 0.5, -1), (1.35, 0.5, -1.35), (1, 0.5, -1.5), (1, 0, -2), (1, 0, -3)], k=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40])
     scale = scale * 0.1666666667
@@ -50,7 +64,7 @@ def trapeziumCtrlShape(name, scale=1):
 
     return ctrl
 
-def chestCtrlShape(name, scale=1):
+def chestCtrlShape(name, normalDirection=[1,1,0], scale=1):
     ctrl = pm.circle(n=name, s=10, nr=[0, 0, 1])[0]
 
     pm.move(0, 0, 1, ctrl.cv[3:4], ctrl.cv[8:9], r=True, os=True)
@@ -60,15 +74,22 @@ def chestCtrlShape(name, scale=1):
     pm.move(0, 0, -1, ctrl.cv[1], ctrl.cv[6], r=True, os=True)
     pm.move(0, -0.75, 0, ctrl.cv[1], r=True, os=True)
     pm.move(0, 0.25, 0, ctrl.cv[0], ctrl.cv[2], r=True, os=True)
+    #pm.xform(ctrl, ws=True, pivots=[0, 0, -0.35])
 
-    pm.xform(ctrl, ws=True, pivots=[0, 0, -0.35])
+    if normalDirection[0] == 1:
+        pm.rotate(ctrl.cv[:], [90, 0, 0])
+    if normalDirection[1] == 1:
+        pm.rotate(ctrl.cv[:], [0, 90, 0])
+    if normalDirection[2] == 1:
+        pm.rotate(ctrl.cv[:], [0, 0, 90])
+
     ctrl.scale.set(scale, scale, scale)
     common.deleteHistory(ctrl)
     common.freezeTranform(ctrl)
 
     return ctrl
 
-def hipCtrlShape(name, scale=1):
+def hipCtrlShape(name, normalDirection=[1,1,0], scale=1):
     ctrl = pm.circle(n=name, s=10, nr=[0, 0, 1])[0]
 
     pm.move(0, 0, 1, ctrl.cv[3:4], ctrl.cv[8:9], r=True, os=True)
@@ -77,8 +98,15 @@ def hipCtrlShape(name, scale=1):
     pm.move(0, 0, -0.75, ctrl.cv[0], ctrl.cv[2], ctrl.cv[5], ctrl.cv[7], r=True, os=True)
     pm.move(0, 0, -1, ctrl.cv[1], ctrl.cv[6], r=True, os=True)
     pm.scale(ctrl.cv[:], [1, 0.5, 1], r=True, p=[0, 0, 0])
+    #pm.xform(ctrl, ws=True, pivots=[0, 0, -0.15])
 
-    pm.xform(ctrl, ws=True, pivots=[0, 0, -0.15])
+    if normalDirection[0] == 1:
+        pm.rotate(ctrl.cv[:], [90, 0, 0])
+    if normalDirection[1] == 1:
+        pm.rotate(ctrl.cv[:], [0, 90, 0])
+    if normalDirection[2] == 1:
+        pm.rotate(ctrl.cv[:], [0, 0, 90])
+
     ctrl.scale.set(scale, scale, scale)
     common.deleteHistory(ctrl)
     common.freezeTranform(ctrl)
