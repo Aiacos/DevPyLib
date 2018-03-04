52df9fd33653a1dfd7d60037853a4114f176caae
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2018-03-04 19:19:26 +0000

    tail integration

--- mayaLib/rigLib/base/rig.py
+++ mayaLib/rigLib/base/rig.py
@@ -9,6 +9,7 @@ from mayaLib.rigLib.utils import util
 from mayaLib.rigLib.base import control
 from mayaLib.rigLib.base import spine
 from mayaLib.rigLib.base import neck
+from mayaLib.rigLib.base import ikChain
 
 from mayaLib.rigLib.utils import proxyGeo
 
@@ -86,43 +87,58 @@ class Rig(BaseRig):
     """
     def __init__(self, characterName='new',
                  model_filePath='', buildScene_filePath='',
+                 sceneScale=1,
                  rootJnt='spineJA_JNT',
                  headJnt='headJA_JNT',
                  loadSkinCluster=True,
                  doProxyGeo=True,
                  doSpine=True,
                  doNeck=True,
+                 doTail=True, doDynamicTail=False,
                  ):
         """
         Create Base Rig
         :param characterName: str
         :param model_filePath: str
         :param buildScene_filePath: str
+        :param sceneScale: float
         :param rootJnt: str
         :param headJnt: str
         :param loadSkinCluster: bool
         :param doProxyGeo: bool
         :param doSpine: bool
         :param doNeck: bool
+        :param doTail: bool
         """
         super(Rig, self).__init__(characterName, model_filePath, buildScene_filePath, rootJnt, headJnt, doProxyGeo, loadSkinCluster)
 
         if doSpine:
             spineJoints = pm.ls('spine*_JNT')
             #spineJoints = ['spine1_jnt', 'spine2_jnt', 'spine3_jnt', 'spine4_jnt', 'spine5_jnt', 'spine6_jnt']
-            spineRig = spine.Spine(spineJoints, rootJnt, prefix='spine', rigScale=1.0, baseRig=self.baseModule)
+            spineRig = spine.Spine(spineJoints, rootJnt, prefix='spine', rigScale=sceneScale, baseRig=self.baseModule)
 
         if doNeck:
             neckJoints = pm.ls('neck*_JNT')
             #neckJoints = ['neck1_jnt', 'neck2_jnt', 'neck3_jnt', 'neck4_jnt', 'neck5_jnt', 'neck6_jnt']
-            neckRig = neck.Neck(neckJoints, headJnt, prefix='neck', rigScale=1.0, baseRig=self.baseModule)
+            neckRig = neck.Neck(neckJoints, headJnt, prefix='neck', rigScale=sceneScale, baseRig=self.baseModule)
 
         if doSpine and doNeck:
             pm.parentConstraint(spineJoints[-1], neckRig.getModuleDict()['baseAttachGrp'], mo=1)
             pm.parentConstraint(spineRig.getModuleDict()['bodyCtrl'].C, neckRig.getModuleDict()['bodyAttachGrp'], mo=1)
 
+        if doTail:
+            tailJoints = pm.ls('tail*_JNT')
+            pelvisJnt = pm.ls('pelvis*_JNT')[-1]
+            tailRig = ikChain.IKChain(
+                chainJoints=tailJoints,
+                prefix='tail',
+                rigScale=sceneScale,
+                doDynamic=doDynamicTail,
+                smallestScalePercent=0.4,
+                fkParenting=False,
+                baseRig=self.baseModule)
 
-
+            pm.parentConstraint(pelvisJnt, tailRig.getModuleDict()['baseAttachGrp'], mo=1)
 
 
 if __name__ == "__main__":
