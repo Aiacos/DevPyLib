8c407a30c4ba69b32c93722ae2330756157575e8
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2018-12-02 01:49:26 +0000

    Fix mayaLib widget
    Fix mayaLib widget

--- mayaLib/guiLib/mainMenu.py
+++ mayaLib/guiLib/mainMenu.py
@@ -262,6 +262,7 @@ class MainMenu(QtWidgets.QWidget):
         self.libMenu.addAction(self.wAction)
 
         QObject.connect(self.libWindow, SIGNAL('updateWidget()'), lambda: self.updateWidget(libPath))
+        self.libMenu.triggered.connect(self.showWidget)
 
     def updateWidget(self, libPath):
         reload_package(mayaLib)
@@ -276,7 +277,11 @@ class MainMenu(QtWidgets.QWidget):
 
         self.libMenu.addAction(self.wAction)
         QObject.connect(self.libWindow, SIGNAL('updateWidget()'), lambda: self.updateWidget(libPath))
+        self.libMenu.triggered.connect(self.showWidget)
         print 'Reloaded MayaLib!'
+
+    def showWidget(self):
+        self.libWindow.adjustSize()
 
     def __del__(self):
         self.libMenu.deleteLater()
