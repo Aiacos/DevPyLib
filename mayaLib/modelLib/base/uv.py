56b03219a9bb2aed1b1641d961daaad3cfe69259
Lorenzo <uni.lorenzo.a@gmail.com>
2019-03-04 23:48:46 +0000

    AutoUV to Class

--- mayaLib/modelLib/base/uv.py
+++ mayaLib/modelLib/base/uv.py
@@ -4,180 +4,183 @@ import maya.mel as mel
 from mayaLib.rigLib.utils.util import getAllObjectUnderGroup
 
 
-def checkUVInBoundaries(shell):
-    uvs = pm.polyListComponentConversion(shell, tuv=True)
+class AutoUV():
 
-    uMax = 1
-    uMin = 0
-    vMax = 1
-    vMin = 0
-    for i, uv in zip(range(len(pm.ls(uvs, fl=True))), pm.ls(uvs, fl=True)):
-        u, v = pm.polyEditUV(uv, q=True, u=True, v=True)
+    def __init__(self, geoList=pm.ls(sl=True), mapRes=1024, texelDensity=16, autoSeamAngle=0,
+                 autoProject=True, autoSeam=True, autoCutUV=True):
+        """
 
-        if i > 0:
-            if (u > uMin) and (u < uMax) and (v > vMin) and (v < vMax):
-                pass
-                # return True
-            else:
-                return False
+        :param geoList:
+        :param mapRes:
+        :param texelDensity:
+        :param autoSeamAngle:
+        :param autoProject:
+        :param autoSeam:
+        :param autoCutUV:
+        """
 
-        uMax = int(u) + 1
-        uMin = int(u)
+        area = 0
 
-        vMax = int(v) + 1
-        vMin = int(v)
+        for geo in geoList:
+            print 'Current Geo: ', geo.name()
 
-    return True
+            # fi non Manifold UV
+            self.fixNonManifoldUV(geo)
 
+            # Automatic Projection UV
+            if autoProject:
+                pm.polyAutoProjection(geo.f[:], lm=0, pb=0, ibd=1, cm=0, l=0, sc=0, o=0, p=6, ps=0.2, ws=0)
 
-def checkUVBoundaries(shell):
-    uvs = pm.polyListComponentConversion(shell, tuv=True)
-    uvTileRange = []
+            # Auto Seam 1
+            if autoSeam:
+                self.autoSeamUV(geo, autoSeamAngle)
 
-    for i, uv in zip(range(len(pm.ls(uvs, fl=True))), pm.ls(uvs, fl=True)):
-        u, v = pm.polyEditUV(uv, q=True, u=True, v=True)
+            # Unfold3D Optimize
+            self.unfoldOptimizeUV(geo)
 
-        uMax = int(u) + 1
-        uMin = int(u)
+            # Layout
+            pm.u3dLayout(geo, res=256, mutations=1, rot=2, scl=1, box=[0, 1, 0, 1])
 
-        vMax = int(v) + 1
-        vMin = int(v)
+            # set Texel Density
+            self.setTexelDensity(geo, texelDensity, mapRes)
 
-        tile = [uMin, uMax, vMin, vMax]
-        if tile not in uvTileRange:
-            uvTileRange.append([uMin, uMax, vMin, vMax])
+            # check UV boundaries
+            if autoCutUV:
+                self.recursiveCutUV(geo)
 
-    return uvTileRange
+            # delete history
+            pm.delete(geo, ch=1)
 
+            area = area + pm.polyEvaluate(geo, uvArea=True)
 
-def cutUVTile(shell):
-    for tile in checkUVBoundaries(shell):
-        tmpBuffer = []
+        print 'Total Area: ', area, ' -- RoundUp: ', math.ceil(area)
+        # Layout with TexelDensity
+        self.finalLayoutUV(geoList, area)
+        # pm.select(geoList)
+
+        print 'Auto UV Complete!'
+
+    def checkUVInBoundaries(self, shell):
         uvs = pm.polyListComponentConversion(shell, tuv=True)
 
-        for uv in pm.ls(uvs, fl=True):
+        uMax = 1
+        uMin = 0
+        vMax = 1
+        vMin = 0
+        for i, uv in zip(range(len(pm.ls(uvs, fl=True))), pm.ls(uvs, fl=True)):
             u, v = pm.polyEditUV(uv, q=True, u=True, v=True)
-            if u > tile[0] and u < tile[1] and v > tile[2] and v < tile[3]:
-                tmpBuffer.append(uv)
 
-        faces = pm.polyListComponentConversion(pm.ls(tmpBuffer), tuv=True)
-        pm.select(faces)
-        mel.eval('CreateUVShellAlongBorder;')
-        # pm.polyMapCut(faces, ch=True)
+            if i > 0:
+                if (u > uMin) and (u < uMax) and (v > vMin) and (v < vMax):
+                    pass
+                    # return True
+                else:
+                    return False
 
+            uMax = int(u) + 1
+            uMin = int(u)
 
-def recursiveCutUV(geo):
-    shellList = getUVShell(geo)
-    for shell in shellList:
-        if not checkUVInBoundaries(shell):
-            cutUVTile(shell)
+            vMax = int(v) + 1
+            vMin = int(v)
 
+        return True
 
-def getUVShell(geo):
-    shellNumber = pm.polyEvaluate(geo, uvShell=True)
-    shellList = []
-    for i in range(shellNumber):
-        shellFaces = pm.polyListComponentConversion(pm.polyEvaluate(geo, uvsInShell=i), toFace=True)
-        shellList.append(shellFaces)
+    def checkUVBoundaries(self, shell):
+        uvs = pm.polyListComponentConversion(shell, tuv=True)
+        uvTileRange = []
 
-    return shellList
+        for i, uv in zip(range(len(pm.ls(uvs, fl=True))), pm.ls(uvs, fl=True)):
+            u, v = pm.polyEditUV(uv, q=True, u=True, v=True)
 
+            uMax = int(u) + 1
+            uMin = int(u)
 
-def setTexelDensity(geo, texelDensity=10.24, mapRes=1024):
-    texSetTexelDensity = 'texSetTexelDensity ' + str(texelDensity) + ' ' + str(mapRes) + ';'
-    pm.select(geo.f[:])
-    mel.eval(texSetTexelDensity)
+            vMax = int(v) + 1
+            vMin = int(v)
 
+            tile = [uMin, uMax, vMin, vMax]
+            if tile not in uvTileRange:
+                uvTileRange.append([uMin, uMax, vMin, vMax])
 
-def uvLayoutNoScale(geoList, uCount, vCount, mapRes=1024, iteration=2):
-    pm.u3dLayout(geoList, res=mapRes, mutations=iteration, rot=2, box=[0, 1, 0, 1], shellSpacing=0.0009765625,
-                 tileMargin=0.001953125, layoutScaleMode=1, u=uCount, v=vCount, rst=90, rmn=0, rmx=360)
+        return uvTileRange
 
+    def cutUVTile(self, shell):
+        for tile in self.checkUVBoundaries(shell):
+            tmpBuffer = []
+            uvs = pm.polyListComponentConversion(shell, tuv=True)
 
-def finalLayoutUV(geoList, area=1):
-    tileNumber = math.ceil(area)
-    tileValue = tileNumber / 2
-    uCount = tileValue if tileValue % 2 else tileValue + 1
-    vCount = tileValue + 1
+            for uv in pm.ls(uvs, fl=True):
+                u, v = pm.polyEditUV(uv, q=True, u=True, v=True)
+                if u > tile[0] and u < tile[1] and v > tile[2] and v < tile[3]:
+                    tmpBuffer.append(uv)
 
-    print 'UV: ', math.ceil(uCount), math.ceil(vCount)
-    uvLayoutNoScale(geoList, math.ceil(uCount), math.ceil(vCount))
+            faces = pm.polyListComponentConversion(pm.ls(tmpBuffer), tuv=True)
+            pm.select(faces)
+            mel.eval('CreateUVShellAlongBorder;')
+            # pm.polyMapCut(faces, ch=True)
 
-    badShellList = []
-    for geo in geoList:
-        for shell in getUVShell(geo):
-            if not checkUVInBoundaries(shell):
-                badShellList.append(shell)
-    if len(badShellList) > 0:
-        print 'Bad Shells'
-        uvLayoutNoScale(badShellList, 1, 1)
-        pm.polyEditUV(badShellList, u=0, v=vCount)
-
-    print 'Auto UV Complete!'
-
-
-def autoSeamUV(geo, angle=0):
-    try:
-        pm.u3dAutoSeam(geo, s=angle, p=1)  # s=0
-    except:
-        pm.unfold(geo)
-
-
-def unfoldOptimizeUV(geo, normalizeShell=False):
-    pm.u3dUnfold(geo, mapsize=1024, iterations=2, pack=0, borderintersection=True, triangleflip=True, roomspace=0)
-    if normalizeShell:
-        shellList = getUVShell(geo)
+    def recursiveCutUV(self, geo):
+        shellList = self.getUVShell(geo)
         for shell in shellList:
-            pm.polyNormalizeUV(shell, normalizeType=1, preserveAspectRatio=False, centerOnTile=True,
-                               normalizeDirection=0)
+            if not self.checkUVInBoundaries(shell):
+                self.cutUVTile(shell)
 
+    def getUVShell(self, geo):
+        shellNumber = pm.polyEvaluate(geo, uvShell=True)
+        shellList = []
+        for i in range(shellNumber):
+            shellFaces = pm.polyListComponentConversion(pm.polyEvaluate(geo, uvsInShell=i), toFace=True)
+            shellList.append(shellFaces)
 
-def fixNonManifoldUV(geo):
-    pm.select(geo)
-    mel.eval(
-        'polyCleanupArgList 4 { "0","1","0","0","0","0","0","0","0","1e-05","0","1e-05","0","1e-05","0","1","0","0" };')
+        return shellList
 
+    def setTexelDensity(self, geo, texelDensity=10.24, mapRes=1024):
+        texSetTexelDensity = 'texSetTexelDensity ' + str(texelDensity) + ' ' + str(mapRes) + ';'
+        pm.select(geo.f[:])
+        mel.eval(texSetTexelDensity)
 
-def autoUV(geoList=pm.ls(sl=True), mapRes=1024, texelDensity=32, autoSeam=0):
-    # Default TexelDensity
-    # texelDensity = mapRes/100;
-    area = 0
+    def uvLayoutNoScale(self, geoList, uCount, vCount, mapRes=1024, iteration=2):
+        pm.u3dLayout(geoList, res=mapRes, mutations=iteration, rot=2, box=[0, 1, 0, 1], shellSpacing=0.0009765625,
+                     tileMargin=0.001953125, layoutScaleMode=1, u=uCount, v=vCount, rst=90, rmn=0, rmx=360)
 
-    for geo in geoList:
-        print geo
-        # Automatic Projection UV
-        pm.polyAutoProjection(geo.f[:], lm=0, pb=0, ibd=1, cm=0, l=0, sc=0, o=0, p=6, ps=0.2, ws=0)
+    def finalLayoutUV(self, geoList, area=1):
+        tileNumber = math.ceil(area)
+        tileValue = tileNumber / 2
+        uCount = tileValue if tileValue % 2 else tileValue + 1
+        vCount = tileValue + 1
 
-        # fi non Manifold UV
-        fixNonManifoldUV(geo)
+        print 'UV: ', math.ceil(uCount), math.ceil(vCount)
+        self.uvLayoutNoScale(geoList, math.ceil(uCount), math.ceil(vCount))
 
-        # Auto Seam 1
-        autoSeamUV(geo, autoSeam)
+        badShellList = []
+        for geo in geoList:
+            for shell in self.getUVShell(geo):
+                if not self.checkUVInBoundaries(shell):
+                    badShellList.append(shell)
+        if len(badShellList) > 0:
+            print 'Bad Shells'
+            self.uvLayoutNoScale(badShellList, 1, 1)
+            pm.polyEditUV(badShellList, u=0, v=vCount)
 
-        # Unfold3D Optimize
-        unfoldOptimizeUV(geo)
+    def autoSeamUV(self, geo, angle=0):
+        try:
+            pm.u3dAutoSeam(geo, s=angle, p=1)  # s=0
+        except:
+            pm.unfold(geo)
 
-        # Layout
-        pm.u3dLayout(geo, res=256, mutations=1, rot=2, scl=1, box=[0, 1, 0, 1])
+    def unfoldOptimizeUV(self, geo, normalizeShell=False):
+        pm.u3dUnfold(geo, mapsize=1024, iterations=2, pack=0, borderintersection=True, triangleflip=True, roomspace=0)
+        if normalizeShell:
+            shellList = self.getUVShell(geo)
+            for shell in shellList:
+                pm.polyNormalizeUV(shell, normalizeType=1, preserveAspectRatio=False, centerOnTile=True,
+                                   normalizeDirection=0)
 
-        # set Texel Density
-        setTexelDensity(geo, texelDensity, mapRes)
-
-        # check UV boundaries
-        recursiveCutUV(geo)
-
-        # delete history
-        pm.delete(geo, ch=1)
-
-        area = area + pm.polyEvaluate(geo, uvArea=True)
-
-    print 'Total Area: ', area, ' -- RoundUp: ', math.ceil(area)
-
-    # Layout with TexelDensity
-    finalLayoutUV(geoList, area)
-    pm.select(geoList)
+    def fixNonManifoldUV(self, geo):
+        pm.select(geo)
+        mel.eval('polyCleanupArgList 4 { "0","1","0","0","0","0","0","0","0","1e-05","0","1e-05","0","1e-05","0","1","0","0" };')
 
 
 if __name__ == "__main__":
-    geos = getAllObjectUnderGroup(pm.ls(sl=True)[0])
-    autoUV(geos, texelDensity=32)
+    geos = pm.ls(sl=True)
+    a = AutoUV(geos)
