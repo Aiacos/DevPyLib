b71ae2136a072e340bbfb0aecef59db6c8a037e7
Aiacos <uni.lorenzo.a@gmail.com>
2023-09-14 09:57:51 +0200

    Fix

--- mayaLib/rigLib/utils/deform.py
+++ mayaLib/rigLib/utils/deform.py
@@ -19,7 +19,7 @@ def createProximityWrap(source, target_list):
     # implementing with maya.cmds since PyMel raises the following warning for every attribute set.
     # Warning: pymel.core.general : Could not create desired MFn. Defaulting to MFnDependencyNode.
     target_names_list = [geo.name() for geo in pm.ls(target_list)]
-    deformer = cmds.deformer(target_names_list, type='proximityWrap', name=target_list[0].name(stripNamespace=True) + '_pWrap')[0]
+    deformer = cmds.deformer(target_names_list, type='proximityWrap', name=pm.ls(target_list)[0].name(stripNamespace=True) + '_pWrap')[0]
 
     proximity_interface = node_interface.NodeInterface(deformer)
     proximity_interface.addDriver(source.getShapes()[-1].name())  # last shape should be the deformed shape
