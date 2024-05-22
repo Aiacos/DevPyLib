cbc7f40ba6e9c98e91b5171b8c009bfa90ee6dee
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2024-05-22 19:07:24 +0200

    Added USD comments

--- mayaLib/rigLib/utils/usd.py
+++ mayaLib/rigLib/utils/usd.py
@@ -31,7 +31,17 @@ def getAllObjectUnderGroup(group, type='mesh'):
 
     return objList
     
-def get_all_deformerd_and_constrained(group):
+def get_all_deformed_and_constrained(group):
+    """
+    Get all mesh deformed and constrained under a group
+    Args:
+        group (string): root group name
+
+    Returns:
+        (string[]): list of mesh with deformer
+        (string[]): list of mesh without deformer
+
+    """
     mesh_list = getAllObjectUnderGroup(group, type='mesh')
     
     deformed_list = []
@@ -53,6 +63,15 @@ class USDCharacterBuild(object):
     """
 
     def __init__(self, geo_list=[], name='', root_node='', connect_output=True, debug=False):
+        """
+        Constructor
+        Args:
+            geo_list (string[]): list of geos
+            name (string): name of the bifrost graph
+            root_node (string):
+            connect_output (bool):
+            debug (bool):
+        """
         self.bifrost_shape, self.bifrost_transform = self.create_bifrost_graph(name)
         self.add_to_stage_node, self.time_node, self.save_usd_stage_node = self.create_default_usd_stage()
         
@@ -78,25 +97,70 @@ class USDCharacterBuild(object):
         
 
     def create_bifrost_graph(self, name='usd'):
+        """
+        Create bifrost Graph
+        Args:
+            name (string): Name of the Graph
+
+        Returns:
+            (string): bifrost shape node
+            (string): bifrost transform node
+
+        """
         bifrost_shape = bifrost.create_bifrost_graph(name)
         bifrost_transform = cmds.listRelatives(bifrost_shape, p=True)[-1]
 
         return bifrost_shape, bifrost_transform
         
     def get_bifrost_transform(self):
+        """
+        Get Maya bifrost transform
+        Returns:
+            (string): Maya bifrost transform
+
+        """
         return self.bifrost_transform
         
     def connect_output(self):
+        """
+        Connect bifrost output node
+        Returns:
+
+        """
         bifrost.bf_add_input_port(self.bifrost_shape, 'output', 'out_stage', 'BifrostUsd::Stage')
         bifrost.bf_connect(self.bifrost_shape, self.save_usd_stage_node + '.out_stage', 'output.out_stage')
         
     def set_start_frame(self, frame):
+        """
+        Set Start Frame in the bifrost input
+        Args:
+            frame (float): start frame
+
+        Returns:
+
+        """
         cmds.setAttr(self.bifrost_shape + ".start_frame", frame)
 
     def set_end_frame(self, frame):
+        """
+        Set End Frame in the bifrost input
+        Args:
+            frame (float): end frame
+
+        Returns:
+
+        """
         cmds.setAttr(self.bifrost_shape + ".end_frame", frame)
                 
     def create_default_usd_stage(self):
+        """
+        Create default Bifrost USD stage
+        Returns:
+            (string): bifrost Add to Stage node name
+            (string): bifrost Time node name
+            (string): bifrost Save Stage node name
+
+        """
         # Add Input Port
         bifrost.bf_add_output_port(self.bifrost_shape, 'input', 'layer', 'string') # node, port_name, port_type
         bifrost.bf_add_output_port(self.bifrost_shape, 'input', 'start_frame', 'float')
@@ -132,6 +196,16 @@ class USDCharacterBuild(object):
         return add_to_stage_node, time_node, save_usd_stage_node
         
     def create_prim(self, obj, prim_type="Xform"):
+        """
+        Create bifrost prim from Maya object
+        Args:
+            obj (string): Maya object name
+            prim_type (string): type of the prim
+
+        Returns:
+            (string): bifrost node name of the prim
+
+        """
         # List all Nodes in the Grpah
         graph_node_list = cmds.vnnCompound(self.bifrost_shape, "/", listNodes=True)
         
@@ -152,6 +226,14 @@ class USDCharacterBuild(object):
         return node
         
     def add_mesh(self, mesh_name):
+        """
+        Add Mesh shape to bifrost and relative mesh prim definition
+        Args:
+            mesh_name (string): Maya mesh name
+
+        Returns:
+
+        """
         # Creaci una classe
         input_mesh_node = bifrost.bf_add_mesh(self.bifrost_shape, mesh_name)
         define_mesh_node = bifrost.bf_create_node(self.bifrost_shape, "BifrostGraph,USD::Prim,define_usd_mesh")
@@ -166,6 +248,16 @@ class USDCharacterBuild(object):
         self.recursive_build_usd_graph(mesh_name, define_mesh_node)
         
     def recursive_build_usd_graph(self, obj, node):
+        """
+        Recursive build and connect Prims
+        Args:
+            obj (string): current Maya object
+            node (string): current Bifrost prim node
+
+        Returns:
+            None
+
+        """
         if obj:
             parent = cmds.listRelatives(obj, p=True)[-1]
             
@@ -196,7 +288,7 @@ class USDCharacterBuild(object):
 if __name__ == "__main__":
     to_delete = cmds.ls('*_bifrostGraph?' 'mayaUsdProxy?')
     cmds.delete(to_delete)
-    deformed_list, undeformed_list = get_all_deformerd_and_constrained('root')
+    deformed_list, undeformed_list = get_all_deformed_and_constrained('root')
     print('Deformed list: ', deformed_list)
     print('Undeformed list: ', undeformed_list)
     usd_character_manager = USDCharacterBuild(getAllObjectUnderGroup('root', type='mesh'), name='test')
